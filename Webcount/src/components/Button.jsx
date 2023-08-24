3eedaefd5de102bc624a87185679d42255c8948f
juangus2345 <119700400+juangus2345@users.noreply.github.com>
2023-08-24 14:29:03 -0500

    Entrega Contador web

---
+++ Webcount/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import '../style/Button.css'
+
+function Button(props) {
+  
+    return (
+      <>
+        <button id="btn"></button> 
+      </>
+    )
+  }
+  
+  export default Button
+  
---
+++ Webcount/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../style/Header.css'
+
+function Header() {
+
+  return (
+    <>
+      <h1>Counter</h1>  
+    </>
+  )
+}
+
+export default Header
